58bc8e273b785d7be31f1f67da107db4d7f94f62
klik2x <hijr.time@gmail.com>
2026-02-12 20:54:08 +0700

    Update Dashboard.tsx

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
