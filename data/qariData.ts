07bc2692199381e514c8185a9e409459d66898eb
klik2x <hijr.time@gmail.com>
2026-02-12 21:16:18 +0700

    Create qariData.ts

---
+++ data/qariData.ts
@@ -0,0 +1,40 @@
+// data/qariData.ts
+import { Qari } from '../types';
+
+export const QARI_LIST: Qari[] = [
+  {
+    identifier: 'ar.alafasy',
+    name: 'مشاري راشد العفاسي',
+    englishName: 'Mishary Rashid Alafasy',
+    format: 'audio',
+    type: 'surahbyayah'
+  },
+  {
+    identifier: 'ar.hunafakhalilhussary',
+    name: 'محمود خليل الحصري',
+    englishName: 'Mahmoud Khalil Al-Hussary',
+    format: 'audio',
+    type: 'surahbyayah' // Sangat direkomendasikan untuk lansia karena artikulasi jelas & lambat
+  },
+  {
+    identifier: 'ar.abdulsamad',
+    name: 'عبد الباسط عبد الصمد',
+    englishName: 'Abdul Basit Abdul Samad',
+    format: 'audio',
+    type: 'surahbyayah'
+  },
+  {
+    identifier: 'ar.minshawi',
+    name: 'محمد صديق المنشاوي',
+    englishName: 'Mohamed Siddiq El-Minshawi',
+    format: 'audio',
+    type: 'surahbyayah'
+  },
+  {
+    identifier: 'ar.abdurrahmanaasudais',
+    name: 'عبد الرحمن السديس',
+    englishName: 'Abdurrahman As-Sudais',
+    format: 'audio',
+    type: 'surahbyayah'
+  }
+];
