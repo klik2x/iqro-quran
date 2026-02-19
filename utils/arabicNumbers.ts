bcfa3a018623ab619d73a567c3325cf0d3cb7664
klik2x <hijr.time@gmail.com>
2026-02-19 15:32:04 +0700

    Add files via upload

--- utils/arabicNumbers.ts
+++ utils/arabicNumbers.ts
@@ -1,4 +1,1 @@
-export const toArabicNumerals = (num: number): string => {
-  const arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
-  return num.toString().split('').map(digit => arabicDigits[parseInt(digit)]).join('');
-};
+Permission denied
