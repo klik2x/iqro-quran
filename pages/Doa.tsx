858eebad06cf8348adacd52a1850dea5935ea144
klik2x <hijr.time@gmail.com>
2026-02-19 15:30:47 +0700

    Add files via upload

--- pages/Doa.tsx
+++ pages/Doa.tsx
@@ -1,16 +1,1 @@
-
-
-import React from 'react';
-import DoaModule from '../components/DoaModule';
-import { useTranslation, TranslationKeys } from '../contexts/LanguageContext'; // NEW: Import TranslationKeys
-
-const DoaPage: React.FC = () => {
-  const { t } = useTranslation();
-  return (
-    <div className="max-w-4xl mx-auto space-y-6 pb-20 px-4">
-      <DoaModule />
-    </div>
-  );
-};
-
-export default DoaPage;
+Permission denied
